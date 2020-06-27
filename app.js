2811d63b45e9b6e29092ffac11748d15620b3d2a
Heng <bunsanphe@gmail.com>
2020-06-27 10:05:34 -0700

    added updated emprole fx

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ function addDept() {
             connection.query("INSERT INTO department (name) VALUES (?)", res.deptName, (err, result) => {
                 if (err) throw err;
                 console.log("Inserted as ID " + result.insertId)
+                addRole()
             })
-            console.log(res)
         })
 }
 
@@ -28,12 +28,12 @@ function addRole() {
         inquirer
         .prompt([
             {
-                message: "What is the title?",
+                message: "Add a title/role.",
                 type: "input",
                 name: "title"
             },
             {
-                message: "What is the salary?",
+                message: "What is the salary for this role?",
                 type: "input",
                 name: "salary",
                 validate: (value) => {
@@ -56,6 +56,7 @@ function addRole() {
             connection.query("INSERT INTO role SET ?", res, (err, result) => {
                 if (err) throw err;
                 console.log("Inserted as ID " + result.insertId)
+                startingPrompt()
             })
         })
     })
@@ -101,6 +102,7 @@ function addEmp() {
             connection.query("INSERT INTO employee SET ?", res, (err, result) => {
                 if (err) throw err;
                 console.log("Inserted as ID " + result.insertId)
+                startingPrompt()
             })
         })
     })
@@ -110,7 +112,7 @@ function viewDept() {
     connection.query("SELECT name FROM department", (err, res) => {
         if (err) throw err;
         console.log(res);
-        connection.end();
+        startingPrompt()
     })
 
 }
@@ -127,11 +129,12 @@ function viewEmp() {
     connection.query("SELECT * FROM employee", (err, res) => {
         if (err) throw err;
         console.log(res);
+        startingPrompt()
     })
 }
 
 function updateEmpRole() {
-    connection.query("SELECT * FROM employee", (err, res) => {
+    connection.query("SELECT first_name, last_name, role_id, title FROM role LEFT JOIN employee ON role.id = employee.role_id", (err, results) => {
         if (err) throw err;
 
         inquirer
@@ -139,24 +142,83 @@ function updateEmpRole() {
             {
                 message: "Which employee would you like to update?",
                 type: "list",
-                name: "newEmpRole",
+                name: "empToChange",
                 choices: results.map( employee => {
                     return {
                         name: employee.first_name,
-                        value: employee.id
+                        value: employee.first_name
                         }
 
                 })
             },
+            {
+                message: "What is the new role?",
+                type: "list",
+                name: "role_id",
+                choices: results.map( role => {
+                    return {
+                        name: role.title,
+                        value: role.id
+                    }
+                }),
+            },
         ])
         .then( (res) => {
-            
+            console.log(res);
+            console.log(res.role);
+            connection.query("UPDATE employee SET role_id = ? WHERE first_name = ?", [res.role_id, res.empToChange], (err, results) => {
+                if (err) throw err;
+                console.log("Inserted as ID " + results.insertId);
+                console.log("success")
+            })
         })
     })
 }
 
 function startingPrompt() {
-
+    inquirer
+    .prompt([
+        {
+            message: "What would you like to do?",
+            type: "list",
+            name: "prompt",
+            choices: [
+                "Add department", 
+                "Add role", 
+                "Add employee", 
+                "View departments", 
+                "View roles",
+                "View employees",
+                "Update employee role",
+            ]
+        },
+    ])
+    .then( (res) => {
+        if (res.prompt === "Add department") {
+            addDept()
+        }
+        else if (res.prompt === "Add role") {
+            addRole()
+        }
+        else if (res.prompt === "Add employee") {
+            addEmp()
+        }
+        else if (res.prompt === "View departments") {
+            viewDept()
+        }
+        else if (res.prompt === "View roles") {
+            viewRole()
+        }
+        else if (res.prompt === "View employees") {
+            viewEmp()
+        }
+        else if (res.prompt === "Update employee role") {
+            updateEmpRole()
+        }
+    })
 }
 
-addEmp()
+// addRole()
+// addDept()
+// updateEmpRole()
+viewDept()
